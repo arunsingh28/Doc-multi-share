807130c69ef583988689767f170f60cdd1c56ab2
Arun Singh <arunsingh28aug.as@gmail.com>
2021-12-23 04:32:02 +0530

    Merge http with express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const app = require('express')
 const server = require('http').createServer(app)
+
 const io = require('socket.io')(server,{
     cors:{
         origin:'*'
@@ -8,13 +9,14 @@ const io = require('socket.io')(server,{
 
 
 io.on('connection', socket => {
-    console.log('connection on')
     socket.on('doc', payload => {
         io.emit('doc', payload)
     })
 })
 
-server.listen(process.env.PORT || 5000, () => {
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
     console.log('server runing')
 })
 
